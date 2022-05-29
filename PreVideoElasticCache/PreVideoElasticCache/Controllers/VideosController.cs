81e298b3a690f6cba4ac7741559b9c65580bf224
MRSBrownie06 <braianloza06@gmail.com>
2022-05-29 22:50:07 +0200

    29 de mayo

---
+++ PreVideoElasticCache/PreVideoElasticCache/Controllers/VideosController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PreVideoElasticCache.Models;
+using PreVideoElasticCache.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreVideoElasticCache.Controllers
+{
+    public class VideosController : Controller
+    {
+        private ServiceCacheRedis service;
+        public VideosController(ServiceCacheRedis service)
+        {
+            this.service = service;
+        }
+        public IActionResult CreatePreVideo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePreVideo(preVideo preVideo)
+        {
+            this.service.AgregarPreVideo(preVideo);
+            return View();
+        }
+    }
+}
---
+++ PreVideoElasticCache/PreVideoElasticCache/Helpers/CacheRedisHelper.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PreVideoElasticCache.Helpers
+{
+    public class CacheRedisHelper
+    {
+        private static Lazy<ConnectionMultiplexer> GenerarConexion = new Lazy<ConnectionMultiplexer>(() =>
+        {
+
+        return ConnectionMultiplexer.Connect("");
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get { return GenerarConexion.Value; }
+        }
+    }
+}
